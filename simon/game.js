c8e28c7c0750c134309c6397c1e081d4f39a6923
iwakunibob <rml128+github@gmail.com>
2024-11-28 22:10:39 -0600

    simon

--- simon/game.js
+++ simon/game.js
@@ -1,12 +1,36 @@
 const buttonColors = ["red", "blue", "green", "yellow"]
-let gamePattern = new Array()
-alert(buttonColors)
+const animateDelay = 200
+let gamePattern = new Array();
+let userClickedPattern = new Array();
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    //console.log(userClickedPattern);
+    playSound(userChosenColor);
+});
+
 function nextSequence() {
     let randomNum = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNum]
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(animateDelay).fadeOut(animateDelay).fadeIn(animateDelay);
+    playSound(randomChosenColor);
 }
-for (let i = 0; i < 20; i++) {
-    nextSequence();
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
-console.log(gamePattern)
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, animateDelay);
+}
+  
+// for (let i = 0; i < 10; i++) {
+//     nextSequence();
+// }
+// console.log(gamePattern)
